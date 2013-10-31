a5c0ac53bd43c74034411073a25ba2646741efd8
Josiah Peters <josiahpeters@gmail.com>
2013-10-31 14:13:42 -0600

    Started fleshing out backend services and data arrangement.

---
+++ src/Kleine.Services/Class1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kleine.Services
+{
+    public class DueDate
+    {
+        public DateTime ExpectedDate { get; set; }
+
+        public string BirthTitle { get; set; }
+
+        public string BirthDescription { get; set; }
+    }
+
+    public class GuessProfile
+    {
+        public int Id { get; set; }
+
+        public Guid SessionId { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string Name { get; set; }
+    }    
+
+    public class GuessGender
+    {
+        public Gender GuessValue { get; set; }
+    }
+
+    public class GuessBirthDate
+    {
+        public DateTime GuessValue { get; set; }
+    }
+
+    public class GuessBirthTime
+    {
+        public DateTime GuessValue { get; set; }
+    }
+
+    public class GuessBirthWeight
+    {
+        public Decimal GuessValue { get; set; }
+    }
+
+    public class GuessBirthLength
+    {
+        public Decimal GuessValue { get; set; }
+    }
+
+    public class GuessBirthName
+    {
+        public string GuessValue { get; set; }
+    }
+
+    
+    
+
+    
+
+    //public abstract class GuessType
+    //{
+    //    abstract public string Name { get; set; }
+
+    //    abstract public string Description { get; set; }
+
+    //    abstract public Type DataType { get; }
+
+    //    abstract public object Value { get; set; }
+    //}
+
+
+    //public class GuessGender : GuessType
+    //{
+    //    public override string Name { get; set; }
+
+    //    public override string Description { get; set; }
+
+
+
+    //    public override Type DataType
+    //    {
+    //        get
+    //        {
+    //            return typeof(Gender);
+    //        }
+    //    }
+    //}
+
+    //public class GuessBirthDate : GuessType
+    //{
+    //    public override string Name { get; set; }
+
+    //    public override string Description { get; set; }
+
+    //    public override Type DataType
+    //    {
+    //        get
+    //        {
+    //            return typeof(DateTime);
+    //        }
+    //    }
+    //}
+
+    //public class GuessBirthTime : GuessType
+    //{
+    //    public override string Name { get; set; }
+
+    //    public override string Description { get; set; }
+
+    //    public override Type DataType
+    //    {
+    //        get
+    //        {
+    //            return typeof(DateTime);
+    //        }
+    //    }
+    //}
+
+    //public class GuessBirthWeight : GuessType
+    //{
+    //    public override string Name { get; set; }
+
+    //    public override string Description { get; set; }
+
+    //    public override Type DataType
+    //    {
+    //        get
+    //        {
+    //            return typeof(Decimal);
+    //        }
+    //    }
+    //}
+
+    //public class GuessBirthWeight : GuessType
+    //{
+    //    public override string Name { get; set; }
+
+    //    public override string Description { get; set; }
+
+    //    public override Type DataType
+    //    {
+    //        get
+    //        {
+    //            return typeof(Decimal);
+    //        }
+    //    }
+    //}
+
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
---
+++ src/Kleine/Entities/Entity.cs
@@ -0,0 +1,14 @@
+﻿using ServiceStack.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kleine.Entities
+{
+    public class Entity
+    {
+        [AutoIncrement]
+        public int Id { get; set; }
+    }
+}
