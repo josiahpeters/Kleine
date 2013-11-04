4317635c16a34f5e840fa97685009442da45dd32
Josiah Peters <josiahpeters@gmail.com>
2013-11-03 23:08:58 -0700

    Started trying out angular with TypeScript.

--- src/Kleine.Website/js/Main.ts
+++ src/Kleine.Website/js/Main.ts
@@ -1,8 +1,79 @@
-//module 
-function greeter(person) {
-    return "Hello, " + person;
+/// <reference path='/scripts/typings/angularjs/angular.d.ts' />
+
+module Model 
+{
+    export class DueDate 
+    {
+        Id: string;
+        BirthTitle: string;
+        ExpectedDate: Date;
+    }
 }
 
-var user = "Jane User";
+module Kleine
+{
+    export interface Scope 
+    {
+        newBirthTitle: string;
+        newExpectedDate: string;
+        DueDates: Model.DueDate[];
 
-document.body.innerHTML = greeter(user); 
+        addNewDueDate: Function;
+        deleteDueDate: Function;
+    }
+
+    export class Controller
+    {
+        private httpService: any;
+
+        constructor($scope: Scope, $http: any)
+        {
+            this.httpService = $http;
+
+            var controller = this;
+            
+            $scope.addNewDueDate = function ()
+            {
+                var newDueDate = new Model.DueDate();
+                newDueDate.BirthTitle = $scope.newBirthTitle;
+                newDueDate.ExpectedDate = new Date($scope.newExpectedDate);
+
+                console.log("sdf");
+
+                controller.addDueDate(newDueDate, function ()
+                {
+                    console.log(newDueDate);
+
+
+                    controller.getAllDueDates(function (data)
+                    {
+                        $scope.DueDates = data;
+                    });
+                });
+            };
+
+            $scope.deleteDueDate = function ()
+            {
+
+            };
+        }
+
+        addDueDate(dueDate: Model.DueDate, successCallback: Function): void
+        {
+            console.log(dueDate);
+
+            this.httpService.post("/api/DueDate/Create", dueDate).success(function ()
+            {
+                successCallback();
+            });
+        }
+
+        getAllDueDates(successCallBack: Function): void
+        {
+            this.httpService.get("/api/DueDate/").success(function (data, status)
+            {
+                successCallBack(data);
+            });
+        }
+    }
+}
--- src/Kleine.Website/js/main.js
+++ src/Kleine.Website/js/main.js
@@ -1,9 +1,56 @@
-//module
-function greeter(person) {
-    return "Hello, " + person;
-}
+/// <reference path='/scripts/typings/angularjs/angular.d.ts' />
+var Model;
+(function (Model) {
+    var DueDate = (function () {
+        function DueDate() {
+        }
+        return DueDate;
+    })();
+    Model.DueDate = DueDate;
+})(Model || (Model = {}));
 
-var user = "Jane User";
+var Kleine;
+(function (Kleine) {
+    var Controller = (function () {
+        function Controller($scope, $http) {
+            this.httpService = $http;
 
-document.body.innerHTML = greeter(user);
+            var controller = this;
+
+            $scope.addNewDueDate = function () {
+                var newDueDate = new Model.DueDate();
+                newDueDate.BirthTitle = $scope.newBirthTitle;
+                newDueDate.ExpectedDate = new Date($scope.newExpectedDate);
+
+                console.log("sdf");
+
+                controller.addDueDate(newDueDate, function () {
+                    console.log(newDueDate);
+
+                    controller.getAllDueDates(function (data) {
+                        $scope.DueDates = data;
+                    });
+                });
+            };
+
+            $scope.deleteDueDate = function () {
+            };
+        }
+        Controller.prototype.addDueDate = function (dueDate, successCallback) {
+            console.log(dueDate);
+
+            this.httpService.post("/api/DueDate/Create", dueDate).success(function () {
+                successCallback();
+            });
+        };
+
+        Controller.prototype.getAllDueDates = function (successCallBack) {
+            this.httpService.get("/api/DueDate/").success(function (data, status) {
+                successCallBack(data);
+            });
+        };
+        return Controller;
+    })();
+    Kleine.Controller = Controller;
+})(Kleine || (Kleine = {}));
 //# sourceMappingURL=Main.js.map
