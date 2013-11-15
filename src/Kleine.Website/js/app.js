e85b1cee1797b2f0be7eb8a7a7d2bbe3e35c242e
Josiah Peters <josiahpeters@gmail.com>
2013-11-15 11:32:40 -0700

    Added data transfering functionality.

--- src/Kleine.Website/js/app.js
+++ src/Kleine.Website/js/app.js
@@ -28,7 +28,7 @@ var app = angular.module('kleine', modules)
                         gender: 0,
                         time: 0,
                     };
-                    
+
                 },
                 abstract: true,
             })
@@ -45,7 +45,7 @@ var app = angular.module('kleine', modules)
                         controller: function ($scope, $stateParams)
                         {
                             console.log($scope.results);
-                            $scope.chooseMale = function()
+                            $scope.chooseMale = function ()
                             {
                                 $scope.results.gender = "Male";
                             }
@@ -182,8 +182,8 @@ angular.module('kleine.directives', []).
           //templateUrl: 'partials/slider.html',
           template: '<div class="range-slider"><input type="hidden" ng-model="value" />' +
           '    <div class="slider">{{ value }}' +
-          '        <label class="min">{{ minValue.toFixed(1) + " " + label }}</label>' +
-          '        <label class="max">{{ maxValue.toFixed(1) + " " + label }}</label>' +
+          '        <label class="min">{{ minValue + " " + label }}</label>' +
+          '        <label class="max">{{ maxValue + " " + label }}</label>' +
           '    </div></div>',
           scope: {
               name: '=',
@@ -202,13 +202,15 @@ angular.module('kleine.directives', []).
 
               //console.log(scope)
 
-              var startX = 0, startY = 0, x = 0, y = 0;
+              var startX = 0, x = 0, y = 0;
               var minValue = parseFloat(attr.min);
               var maxValue = parseFloat(attr.max);
               var sliderMaxPx = 1140;
               var pixelPerValue = 100;
               var range = parseFloat(attr.range);
               var sliderElement = angular.element(element.children()[1]);
+
+              var target = (attr.target || "value").split('.');
 
               angular.element($window).bind('resize', resize);
 
@@ -236,6 +238,8 @@ angular.module('kleine.directives', []).
                   startX = event.pageX - x;
                   $document.on('mousemove', mousemove);
                   $document.on('mouseup', mouseup);
+
+                  console.log(startX);
               });
 
               // call setup with a delay to prevent conflicting scope.$apply race condition
@@ -244,17 +248,24 @@ angular.module('kleine.directives', []).
               function setup()
               {
                   resize();
-                  scope.$apply(calculateValue());
+                  var targetValue = getTargetValue();
+                  if (targetValue.length > 0)
+                      targetValue = targetValue[0];
+                  scope.$apply(calculateValue(targetValue));
               }
 
               function mousemove(event)
               {
                   x = event.pageX - startX;
 
+                  var startingX = parseFloat(sliderElement.css("left"));
+
                   if (x < 0)
                       x = 0;
                   if (x > sliderMaxPx)
                       x = sliderMaxPx;
+
+                  console.log(startingX, x);
 
                   sliderElement.css({
                       left: x + 'px'
@@ -263,24 +274,66 @@ angular.module('kleine.directives', []).
                   scope.$apply(calculateValue());
               }
 
-              function calculateValue()
+              function calculateValue(value)
               {
+                  if (value !== undefined)
+                  {
+                      if (value < minValue)
+                          value = minValue;
+
+                      if (value > maxValue)
+                          value = maxValue - range;
+
+                      var leftValue = (value - minValue) * pixelPerValue;
+
+                      console.log(value, pixelPerValue, minValue, leftValue);
+                      sliderElement.css({
+                          left: leftValue + 'px'
+                      });
+
+                      x = leftValue;
+                  }
+
                   var left = parseFloat(sliderElement.css("left"));
 
                   if (left == 0)
                       left = 1;
 
-                  scope.minValue = left / pixelPerValue + minValue;
-                  scope.maxValue = left / pixelPerValue + minValue + range;
+                  scope.minValue = (left / pixelPerValue + minValue).toFixed(1);
+                  scope.maxValue = (left / pixelPerValue + minValue + range).toFixed(1);
 
                   scope.value = scope.minValue;
-                  scope.$parent.results["weight"] = scope.value;
-                  //scope.$emit("sliderChange");
+
+                  setTargetValue([scope.minValue, scope.maxValue]);
+              }
+
+              function setTargetValue(value)
+              {
+                  if (target.length > 1)
+                      scope.$parent[target[0]][target[1]] = value;
+                  else
+                      scope.$parent[target] = value;
+              }
+
+              function getTargetValue()
+              {
+                  if (target.length > 1)
+                      return scope.$parent[target[0]][target[1]];
+                  else
+                      return scope.$parent[target];
+              }
+
+              function get(target, properties)
+              {
+                  if (properties.length > 1)
+                  {
+                      return get(target[properties[0]], properties.slice(1));
+                  }
+                  return target[properties[0]];
               }
 
               function resize(event)
               {
-
                   var width = range / (maxValue - minValue);
 
                   sliderElement.css({ width: element[0].offsetWidth * width + 'px' });
