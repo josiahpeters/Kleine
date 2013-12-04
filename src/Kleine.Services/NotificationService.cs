b33fa89fd367c2dcca86294ae6297dd1cfbb8192
Josiah Peters <josiahpeters@gmail.com>
2013-12-03 21:47:49 -0700

    Changed color

--- src/Kleine.Services/NotificationService.cs
+++ src/Kleine.Services/NotificationService.cs
@@ -33,7 +33,7 @@ namespace Kleine
 
             SmtpClient client = new SmtpClient(ipAddress);
 
-            client.Send(message);
+            //client.Send(message);
         }
 
         private static string defaultBody = "<!DOCTYPE HTML PUBLIC '-//W3C//DTD XHTML 1.0 Transitional //EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'> <html> <head> <title></title> <meta http-equiv='Content-Type' content='text/html; charset=utf-8'> <style type='text/css'> body, td {{ font-family: 'Helvetica Neue', Arial, Helvetica, sans-serif; color: #444; }} h1 {{ margin: 8px; font-size: 24px; }} h2 {{ margin: 0 0 10px 0; font-size: 18px; }} p {{ font-size: 13px; line-height: 18px; }} a, a:visited {{ text-decoration: none; color: #3498DB; }} a:hover {{ color: #0086BB; }} .footer a, .footer a:visited {{ text-decoration: none; color: #cccccc; }} .footer a:hover {{ color: #cccccc; }} </style> </head> <body> <table width='100%' cellspacing='0' cellpadding='0' bgcolor='fbfbfb'> <tr> <td align='center'> <table width='640' cellspacing='0' cellpadding='0' style='border: 1px solid #ccc;'> <tr> <td bgcolor='#d3e6a6' align='left' style='padding-left:10px;'> <h1><a href='{2}' style='color: #444444; text-decoration: none; font-size: 16px;'>PreggoPredict</a></h1> </td> </tr> <tr> <td height='10' bgcolor='#bdcda8'></td> </tr> <tr bgcolor='#ffffff'> <td style='padding: 20px' align='left'> <h2>{0}</h2> {1} </td> </tr> <tr align='center' bgcolor='#c5de8b' style='height: 30px;'> <td class='footer' style='color: #777777; font-size: 11px; height: 30px;'>This email was sent to you by <a href='{2}' style='color: #444444;'>PreggoPredict</a>.</td> </tr> </table> </td> </tr> </table> </body> </html>";
