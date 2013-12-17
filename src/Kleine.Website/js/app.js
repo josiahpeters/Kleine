7f00a8bd160180745e01044eb338f58ebb455ee5
Josiah Peters <josiahpeters@gmail.com>
2013-12-17 10:18:34 -0800

    Making more responsive, trying to cut image filesizes.

--- src/Kleine.Website/js/app.js
+++ src/Kleine.Website/js/app.js
@@ -15,6 +15,30 @@ var app = angular.module('kleine', modules)
                 {
                     $scope.profile = profilePrediction.current().Profile;
 
+                    function createSlider()
+                    {
+                        /img/eliana/eliana_01_;
+                    }
+
+                    $('#carousel').flexslider({
+                        animation: "slide",
+                        controlNav: false,
+                        animationLoop: false,
+                        slideshow: false,
+                        itemWidth: 210,
+                        itemMargin: 25,
+                        asNavFor: '#slider',
+                    });
+
+                    $('#slider').flexslider({
+                        animation: "slide",
+                        controlNav: false,
+                        animationLoop: false,
+                        slideshow: true,
+                        slideshowSpeed: 2000,
+                        sync: "#carousel"
+                    });
+
                     if ($stateParams.auth != null)
                     {
                         $scope.message = "We were unable to verify your identity, please use the unique link from the email we sent you when you made your prediction to view your results.";
@@ -110,6 +134,42 @@ var app = angular.module('kleine', modules)
                     $scope.PredictionCount = 0;
 
                     $scope.Results = [];
+                    var data = {};
+
+                    
+
+                    function drawCharts()
+                    {
+                        var width = $($(".canvas-container")[0]).width();
+                        var height = width * .7;
+
+                        var context = document.getElementById("genderPie").getContext("2d");
+                        var myNewChart = new Chart(context).Doughnut(data.Gender);
+
+                        var timeChart = document.getElementById("timeChart");
+
+                        timeChart.width = width;
+                        timeChart.height = height;
+                        var timeChart = new Chart(timeChart.getContext("2d")).Line(data.Time, { scaleOverride: true, scaleSteps: 5, scaleStepWidth: 1, scaleStartValue: 0 });
+
+                        var dateChart = document.getElementById("dateChart");
+                        dateChart.width = width;
+                        dateChart.height = height;
+
+                        new Chart(dateChart.getContext("2d")).Line(data.Date, { scaleOverride: true, scaleSteps: 13, scaleStepWidth: 1, scaleStartValue: 0, });
+
+                        var weightChart = document.getElementById("weightChart");
+                        weightChart.width = width;
+                        weightChart.height = height;
+
+                        new Chart(weightChart.getContext("2d")).Line(data.Weight, { scaleOverride: true, scaleSteps: 11, scaleStepWidth: 2, scaleStartValue: 0 })
+
+                        var lengthChart = document.getElementById("lengthChart");
+                        lengthChart.width = width;
+                        lengthChart.height = height;
+
+                        new Chart(lengthChart.getContext("2d")).Line(data.Length, { scaleOverride: true, scaleSteps: 9, scaleStepWidth: 2, scaleStartValue: 0, })
+                    }
 
                     $http.get('/api/results/')
                     .then(function (response)
@@ -121,11 +181,6 @@ var app = angular.module('kleine', modules)
 
                         var femaleColor = "#fc7272";
                         var femaleColorLight = "rgba(254, 189, 189, 0.5)";
-
-                        var height = 200;
-                        var width = 400;
-
-
 
                         if (response.data.GenderResults.length > 0)
                         {
@@ -139,9 +194,8 @@ var app = angular.module('kleine', modules)
                             $scope.Results.push(getData(female, $scope.PredictionCount, 'girl'));
                             $scope.Results.push(getData(male, $scope.PredictionCount, 'boy'));
 
-                            var data = [{ value: $scope.MaleCount, color: maleColor }, { value: $scope.FemaleCount, color: femaleColor }];
-                            var context = document.getElementById("genderPie").getContext("2d");
-                            var myNewChart = new Chart(context).Doughnut(data);
+                            data.Gender = [{ value: $scope.MaleCount, color: maleColor }, { value: $scope.FemaleCount, color: femaleColor }];
+
                         }
                         if (response.data.TimeCounts.length > 0)
                         {
@@ -160,7 +214,7 @@ var app = angular.module('kleine', modules)
                                 femaleData.push(row.FemaleCount);
                             }
 
-                            var data =
+                            data.Time =
                                 {
                                     labels: labels,
                                     datasets:
@@ -182,21 +236,9 @@ var app = angular.module('kleine', modules)
                                         ]
                                 };
 
-                            var timeChart = document.getElementById("timeChart");
-                            timeChart.width = width;
-                            timeChart.height = height;
-                            var context = timeChart.getContext("2d");
 
-                            //new Chart(ctx).Bar(data, options);
-
-                            var timeChart = new Chart(context).Line(data, {
-                                scaleOverride: true,
-                                scaleSteps: 5,
-                                scaleStepWidth: 1,
-                                scaleStartValue: 0,
-                            });
                         }
-                        if (response.data.TimeCounts.length > 0)
+                        if (response.data.DateCounts.length > 0)
                         {
                             var labels = []; //["January", "February", "March", "April", "May", "June", "July"];
                             var maleData = []; //[65, 59, 90, 81, 56, 55, 40];
@@ -216,7 +258,7 @@ var app = angular.module('kleine', modules)
                                 femaleData.push(row.FemaleCount);
                             }
 
-                            var data =
+                            data.Date =
                                 {
                                     labels: labels,
                                     datasets:
@@ -238,17 +280,7 @@ var app = angular.module('kleine', modules)
                                         ]
                                 };
 
-                            var dateChart = document.getElementById("dateChart");
-                            dateChart.width = width;
-                            dateChart.height = height;
-                            var context = dateChart.getContext("2d");
 
-                            var dateChart = new Chart(context).Line(data, {
-                                scaleOverride: true,
-                                scaleSteps: 13,
-                                scaleStepWidth: 1,
-                                scaleStartValue: 0,
-                            })
                         }
                         if (response.data.WeightCounts.length > 0)
                         {
@@ -267,7 +299,7 @@ var app = angular.module('kleine', modules)
                                 femaleData.push(row.FemaleCount);
                             }
 
-                            var data =
+                            data.Weight =
                                 {
                                     labels: labels,
                                     datasets:
@@ -289,17 +321,7 @@ var app = angular.module('kleine', modules)
                                         ]
                                 };
 
-                            var dateChart = document.getElementById("weightChart");
-                            dateChart.width = width;
-                            dateChart.height = height;
-                            var context = dateChart.getContext("2d");
 
-                            var dateChart = new Chart(context).Line(data, {
-                                scaleOverride: true,
-                                scaleSteps: 11,
-                                scaleStepWidth: 2,
-                                scaleStartValue: 0,
-                            })
                         }
 
                         if (response.data.LengthCounts.length > 0)
@@ -319,7 +341,7 @@ var app = angular.module('kleine', modules)
                                 femaleData.push(row.FemaleCount);
                             }
 
-                            var data =
+                            data.Length =
                                 {
                                     labels: labels,
                                     datasets:
@@ -340,19 +362,9 @@ var app = angular.module('kleine', modules)
                                             },
                                         ]
                                 };
-
-                            var dateChart = document.getElementById("lengthChart");
-                            dateChart.width = width;
-                            dateChart.height = height;
-                            var context = dateChart.getContext("2d");
-
-                            var dateChart = new Chart(context).Line(data, {
-                                scaleOverride: true,
-                                scaleSteps: 9,
-                                scaleStepWidth: 2,
-                                scaleStartValue: 0,
-                            })
                         }
+
+                        drawCharts();
                     });
 
                     function getData(result, total, gender)
@@ -379,6 +391,31 @@ var app = angular.module('kleine', modules)
                         };
                     }
 
+                    function resizeend()
+                    {
+                        if (new Date() - rtime < delta)
+                        {
+                            setTimeout(resizeend, delta);
+                        } else
+                        {
+                            timeout = false;
+                            drawCharts();
+                        }
+                    }
+
+                    var rtime = new Date(1, 1, 2000, 12, 0, 0);
+                    var timeout = false;
+                    var delta = 200;
+
+                    angular.element($(window)).bind('resize', function ()
+                    {
+                        rtime = new Date();
+                        if (timeout === false)
+                        {
+                            timeout = true;
+                            setTimeout(resizeend, delta);
+                        };
+                    });
 
                 },
                 resolve: {
