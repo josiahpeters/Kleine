30d35117c480668cd83418dfb1cbd654432a3364
Josiah Peters <josiahpeters@gmail.com>
2014-09-29 10:03:19 -0600

    Modified preggo predict to support dynamic client generation

---
+++ src/Kleine.Data.Sql/Repositories.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using ServiceStack.OrmLite;
+using System.ComponentModel;
+using Kleine.Data.Sql;
+
+namespace Kleine.Data
+{
+    public class SqlRepositories : IRepositories
+    {
+        private IRepository<DueDate> dueDates;
+        private IProfileRepository profile;
+        private IPredictionRepository predictions;
+        private ICookieTrackerRepository cookieTrackers;
+        private IResultsRepository results;
+
+        OrmLiteConnectionFactory dbFactory;
+
+        public SqlRepositories(OrmLiteConnectionFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+            //IDbConnection db
+            this.dueDates = new BaseSqlRepository<DueDate>(this.dbFactory);
+            this.profile = new ProfileSqlRepository(this.dbFactory);
+            this.predictions = new PredictionSqlRepository(this.dbFactory);
+            this.cookieTrackers = new CookieTrackerSqlRepository(this.dbFactory);
+            this.results = new ResultsSqlRepository(this.dbFactory);
+        }
+
+        public void SetUp()
+        {
+            using (var db = dbFactory.OpenDbConnection())
+            {               
+
+                if (!db.TableExists("DueDates"))
+                    db.CreateTable<DueDate>();
+
+                if (!db.TableExists("Profiles"))
+                    db.CreateTable<Profile>();
+
+                if (!db.TableExists("Predictions"))
+                    db.CreateTable<Prediction>();
+
+                if (!db.TableExists("CookieTrackers"))
+                    db.CreateTable<CookieTracker>();
+
+                var dueDate = dueDates.Create(new DueDate
+                {
+                    Name = "BabyP",
+                    Title = "BabyP",
+                    ExpectedDate = new DateTime(2013, 12, 16),
+                    Description = "Baby P is coming soon!"
+                });
+            }
+        }
+        
+        public IRepository<DueDate> DueDates
+        {
+            get
+            {
+                return dueDates;
+            }
+            set
+            {
+                dueDates = value;
+            }
+        }
+
+        public IProfileRepository Profiles
+        {
+            get
+            {
+                return profile;
+            }
+            set
+            {
+                profile = value;
+            }
+        }
+
+
+        public IPredictionRepository Predictions
+        {
+            get
+            {
+                return predictions;
+            }
+            set
+            {
+                predictions = value;
+            }
+        }
+
+        public ICookieTrackerRepository CookieTrackers
+        {
+            get
+            {
+                return cookieTrackers;
+            }
+            set
+            {
+                cookieTrackers = value;
+            }
+        }
+
+
+        public IResultsRepository Results
+        {
+            get
+            {
+                return results;
+            }
+            set
+            {
+                results = value;
+            }
+        }
+    }
+}
--- src/Kleine.Website/js/app.js
+++ src/Kleine.Website/js/app.js
@@ -1,5 +1,28 @@
 ﻿'use strict';
 var complete = false;
+
+var clientDueDate = {
+    Id: 2,
+    ExpectedDate: new Date(14,10,16,0,1,0,0),
+    DeliveredDate: null,
+    DueDateString: "October 16th, 2014",
+    Name: "Baby R",
+    Title: "",
+    Description: "",
+    CouplesNames: "Katie and Zack",
+    CouplesEmailAddress: "",
+    BabyAlias: "Baby Roberts",
+    BabyName: "",
+    WelcomeMessage: "View Baby Registry\n*blurb:* Their last baby was six years ago, and they didn't keep a thing! They are keeping things simple this time around, but could still use your help! They are registered at Target. (A baby shower will be held approximately two weeks after baby's arrival)\n\nCloth Diaper Fund\n*blurb:* For many reasons, they are going cloth this time! It will save the family over $2,000 during the course of the next 2-3 years by diapering \"the old-fashioned way\" - BUT, the initial investment can be a little painful. They would like to raise about $450 to purchase everything they'll need, and every dollar you donate helps!\n\nTake Them a Meal\n*blurb:* If you would like to bless the family by providing them with a meal after baby comes, thank you so much! Any break from meal planning, shopping, or cooking is much appreciated by mom and dad. HOWEVER- please note there is a food sensitivity in the family, and taking them a meal won't be for the faint at heart. Click the link for ALL the details about what foods make the cut, where or how to shop for and prepare a meal for them, and for a sign up list/ calendar for the truly brave souls! Even if its too much for you to tackle (understandably so), bless you anyway, for its the thought that counts!\nLook up: Roberts, Zack and/or Katie\nPassword is: Baby#3",
+    ThankYouMessage: "",
+    EmailGreeting: "",
+    DateFact: "Katie is due on her older brother Andrew's birthday.",
+    GenderFact: "In the United States, boys make up 51.2% of the birth rate.",
+    LengthFact: "According to the <a href=\"http://www.cdc.gov/growthcharts/html_charts/lenageinf.htm\">CDC</a>, the average length of a newborn baby is 19.54 inches.",
+    TimeFact: "According to everyone, most expecting mothers go into labor at the most inconveinent times possible.",
+    WeightFact: "The average U.S. newborn weight in 2008 was 7 pounds, 4.26 ounces (7.26 lbs).",
+};
+
 var modules = ['ngRoute', 'ui.router', 'ngResource', 'ngAnimate', 'kleine.controllers', 'kleine.directives', 'kleine.filters', 'kleine.services'];
 
 var app = angular.module('kleine', modules)
@@ -206,6 +229,9 @@ var app = angular.module('kleine', modules)
         $rootScope.$state.completed = complete;
 
         $rootScope.$stateParams = $stateParams;
+
+        $rootScope.$state.client = clientDueDate;
+
         profilePrediction.fetch();
     })
 
